5ca397f2bd62c83618323b3d7f06b0024259c12d
VikiPicky <v.mitiakova@gmail.com>
2023-07-18 23:24:49 -0400

    db

--- DB.cs
+++ DB.cs
@@ -8,13 +8,23 @@ using MySql.Data.MySqlClient;
 namespace My_Journal
 {
     internal class DB
+
     {
-        MySqlConnection connection = new MySqlConnection("server = localhost; port = 3306; username = root; password = root; database = myjournal ");
+            MySqlConnection connection = new MySqlConnection("server = localhost; port = 3306; username = root; password = root;");
+                
+            String createDatabaseCommand = string.Format("CREATE SCHEMA IF NOT EXISTS myjournal; " +
+            "USE myjournal;" +
+            "CREATE TABLE IF NOT EXISTS `record` (`id` int(10) UNSIGNED NOT NULL, `record` varchar(100) NOT NULL,  `date` date NOT NULL) ;" +
+            "CREATE TABLE IF NOT EXISTS `user` (`id` int(10) UNSIGNED NOT NULL, `name` varchar(50) NOT NULL, `program` varchar(60) NOT NULL, `email` varchar(50) NOT NULL,`password` varchar(15) NOT NULL);");
 
         public void openConnection() {
-            if (connection.State == System.Data.ConnectionState.Closed)
-                connection.Open();
-        }
+            //if (connection.State == System.Data.ConnectionState.Closed)
+            //    connection.Open();
+
+            //MySqlCommand myCommand = new MySqlCommand(createDatabaseCommand, connection);
+            //myCommand.ExecuteNonQuery();
+            
+          }
 
         public void closeConnection()
         {
@@ -24,6 +34,10 @@ namespace My_Journal
 
         public MySqlConnection getConnection()
         {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+            MySqlCommand myCommand = new MySqlCommand(createDatabaseCommand, connection);
+            myCommand.ExecuteNonQuery();
             return (MySqlConnection)connection;
         }
     }
